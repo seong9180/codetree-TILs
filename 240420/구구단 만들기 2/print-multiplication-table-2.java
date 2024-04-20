71fb517b4807e8fa5e9bb7333f5ad04f49e434ea
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-04-20 20:52:56 +0900

    Title: 구구단 만들기 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240420/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = 0; i < b; i++){
+            for(int j = b; j >= a; j--){
+                System.out.print(j + " * " + 2*(i+1) + " = " + (j*2*(i+1)));
+                if(j > a){
+                    System.out.print(" / ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
