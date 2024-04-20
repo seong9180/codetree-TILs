8a2ad1503a8c5ffd9ce005ea1300d2324e175eb5
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-04-20 21:00:33 +0900

    Title: 구구단 만들기 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240420/구구단 만들기 2/print-multiplication-table-2.java
+++ 240420/구구단 만들기 2/print-multiplication-table-2.java
@@ -7,7 +7,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i = 0; i < b; i++){
+        for(int i = 0; i < 4; i++){
             for(int j = b; j >= a; j--){
                 System.out.print(j + " * " + 2*(i+1) + " = " + (j*2*(i+1)));
                 if(j > a){
