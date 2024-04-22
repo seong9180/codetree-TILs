23425fb2f5ca428e70dbeb6ca02776741b18a2e4
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-04-22 21:31:44 +0900

    Title: 두 개의 격자 비교하기, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+
+        for(int i = 0; i < n; i++){ //격자 1 입력
+            for(int j = 0; j < m ; j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++){ //격자 2 입력
+            for(int j = 0; j < m ; j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++){ //격자 1,2 비교 및 결과 격자 출력
+            for(int j = 0; j < m ; j++){
+                if(arr1[i][j] == arr2[i][j]){
+                    System.out.print("0 ");
+                }else{
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
