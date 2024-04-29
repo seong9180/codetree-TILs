21a41552845545328ea3ea2c3ce7f357c81fd6f7
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-04-29 14:00:26 +0900

    Title: 1이 3개 이상 있는 위치, Time: 214ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240429/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,66 @@
+//package study.codetree.mid.dxdy;
+
+import java.util.Scanner;
+
+public class Main {
+    //상하좌우 인접 - dx dy 사용해보기
+    //격자에서의 dx dy = i, j 순서라는 걸 기억하기
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static boolean inRange(int x, int y, int n) {
+        return (0 <= x && x < n && 0 <= y && y < n) ;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); //입력 n
+        int[][] arr = new int[n][n];
+
+        //격자 정보 입력받기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        /* 격자 입력 확인
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        */
+
+        int x = 0;
+        int y = 0;
+        int cnt_one = 0; //인접한 숫자를 세는 변수 초기화
+        int result = 0;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int d = 0; d < 4; d++) {
+                    int nx = x + dx[d];
+                    int ny = y + dy[d];
+
+                    //nx,ny 가 격자 범위 안에 존재하며, 해당 좌표의 값이 1일 때 cnt 증가
+                    if (inRange(nx, ny, n) && arr[nx][ny] == 1) {
+                        cnt_one ++;
+                    }
+                }
+            }
+            //cnt 가 3이상이라면, 문제에서 원하는 조건의 갯수이므로 result를 1 증가
+            if (cnt_one >= 3) {
+                result ++;
+            }
+        }
+
+        //System.out.println("result = " + result);
+        System.out.println(result);
+
+
+    }
+}
