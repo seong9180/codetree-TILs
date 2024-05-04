313ff57c81cb66dc64ac3c90b94ebb390dd14a22
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-04 23:17:53 +0900

    Title: 2개 중 최대, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240504/2개 중 최대/max-of-two-nums.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int max = 0;
+        if(a > b){
+            max = a;
+        }else if(b > a){
+            max = b;
+        }
+
+        System.out.println(max);
+        
+    }
+}
