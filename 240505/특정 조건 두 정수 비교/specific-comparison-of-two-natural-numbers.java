87aa589acb81da728402e0b544c4ad3427cba9be
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-05 23:07:36 +0900

    Title: 특정 조건 두 정수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240505/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if( a < b){
+            System.out.print(1);
+        }else if(a > b){
+            System.out.print(0);
+        }
+
+        System.out.print(" ");
+
+        if (a == b){
+            System.out.println(1);
+        }else if{
+            System.out.println(0);
+        }
+        
+    }
+}
