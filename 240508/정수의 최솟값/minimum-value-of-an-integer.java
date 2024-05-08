fa64276fda4d09f2b5fc4decf5b414b2555d6090
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-08 21:32:28 +0900

    Title: 정수의 최솟값, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int min(int a, int b, int c){
+        int min = 100;
+        
+        if(a <= b && a <= c){
+            min = a;
+        }else if(b <= a && b <= c){
+            min = b;
+        }else if(c <= a && c <= b){
+            min = c;
+        }
+
+        return min;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(min(a,b,c));
+    }
+}
