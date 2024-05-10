58ff5924d0afada43850912c282f87f40090b56f
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-10 21:13:27 +0900

    Title: 1-9 개수 세기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240510/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[10];
+        int[] count = new int[9];
+
+        for(int i = 0 ; i < 10; i++){
+            if(i < n){
+                arr[i] = sc.nextInt();
+            }
+
+            if(arr[i] == 1){
+                count[0]++;
+            }else if(arr[i] == 2){
+                count[1]++;
+            }else if(arr[i] == 3){
+                count[2]++;
+            }else if(arr[i] == 4){
+                count[3]++;
+            }else if(arr[i] == 5){
+                count[4]++;
+            }else if(arr[i] == 6){
+                count[5]++;
+            }else if(arr[i] == 7){
+                count[6]++;
+            }else if(arr[i] == 8){
+                count[7]++;
+            }else if(arr[i] == 9){
+                count[8]++;
+            }
+        }
+
+        for(int i = 0 ; i < 9; i++){
+            System.out.println(count[i]);
+        }
+    }
+}
