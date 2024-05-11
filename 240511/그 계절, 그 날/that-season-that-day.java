1def7c5a83845596178dc0060fd63202f574c7cc
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-11 18:17:17 +0900

    Title: 그 계절, 그 날, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240511/그 계절, 그 날/that-season-that-day.java
+++ 240511/그 계절, 그 날/that-season-that-day.java
@@ -6,7 +6,11 @@ public class Main {
     public static boolean isLeapYear(int y){
         if (y % 4 == 0) {
             if (y % 100 == 0) {
-                return y % 400 == 0;
+                if(y % 400 == 0){
+                    return true;
+                }else{
+                    return false;
+                }
             }else{
                 return true;
             }
