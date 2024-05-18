1803790bb86cc3a3e7b8c54750f1ee8a94afcd10
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-18 16:08:15 +0900

    Title: 행복한 수열의 개수, Time: 282ms, Memory: 20MB, Status: Passed - CodeTree

--- 240518/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240518/행복한 수열의 개수/number-of-happy-sequence.java
@@ -3,16 +3,14 @@
 import java.util.Scanner;
 public class Main {
     public static int n, m;
-    public static int MAX_N = 100;
+    public static final int MAX_N = 100;
     public static int[][] grid = new int[MAX_N][MAX_N];
     public static int[] seq = new int[MAX_N];
 
     public static boolean isHappySequence() {
-        int maxcCount = 1;
-        for (int i = 0; i < n - 1; i++) {
-            int consecutiveCount = 1;
-
-            if (seq[i] == seq[i + 1]) {
+        int maxcCount = 1, consecutiveCount = 1;
+        for (int i = 1; i < n; i++) {
+            if (seq[i-1] == seq[i]) {
                 consecutiveCount ++;
             }else{
                 consecutiveCount = 1; //연속하지 않으면 - 가 아니고 1(초기상태) 로 돌아오게 하기
@@ -47,13 +45,13 @@ public class Main {
         }
 
         //세로열 카운트
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                seq[j] = grid[j][i]; //주의해야 할 부분 !
-            }
-            if (isHappySequence()) {
-                numHappy ++;
-            }
+        for(int j = 0; j < n; j++){
+            // 세로로 숫자들을 모아 새로운 수열을 만듭니다.
+            for(int i = 0; i < n; i++)
+                seq[i] = grid[i][j];
+
+            if(isHappySequence())
+                numHappy++;
         }
 
         System.out.println(numHappy);
