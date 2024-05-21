e0602eeed2d867ead480551363c629bcc1943236
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-21 21:15:06 +0900

    Title: 5의 배수의 합, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240521/5의 배수의 합/sum-of-specific-condition-multiple.java
+++ 240521/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -10,13 +10,13 @@ public class Main {
         int sum = 0;
 
         if(a >= b){
-            for(int i = a; i <= b; i++){
+            for(int i = b; i <= a; i++){
                 if(i % 5 == 0){
                     sum += i;
                 }
             }
         }else{
-            for(int i = b; i <= a; i++){
+            for(int i = a; i <= b; i++){
                 if(i % 5 == 0){
                     sum += i;
                 }
