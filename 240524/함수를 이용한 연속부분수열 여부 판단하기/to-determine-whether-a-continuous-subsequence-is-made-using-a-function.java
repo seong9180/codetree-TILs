ea36ff98b5e164e24e5edf4740dd780480963531
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-24 16:35:02 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240524/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240524/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class Main {
    public static int n1, n2;
    public static final int MAX_N1 = 100, MAX_N2 = 100;
-   public static int[] arr1 = new int[MAX_N1];
-   public static int[] arr2 = new int[MAX_N2];
+   public static int[] arr1 = new int[MAX_N1 + 2];
+   public static int[] arr2 = new int[MAX_N2 + 2];
 
     public static boolean isConsecSubseq() {
         boolean check = false;
         int count = 0;
+        if (n2 > n1) { //B 수열의 크기가 A 수열보다 큰 경우 : 절대 부분수열이 될 수 없음.
+            return false;
+        }
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n2; j++) {
                 if(arr1[i + j] == arr2[j]){
