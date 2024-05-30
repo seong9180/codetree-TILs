21365692ca6c811c707cd613ba8fdcc060739837
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-30 18:08:24 +0900

    Title: 999 또는 -999, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240530/999 또는 -999/999-or-999.java
+++ 240530/999 또는 -999/999-or-999.java
@@ -4,19 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[1000];
-        int max = arr[0], min = arr[0];
+        int[] arr = new int[100];
+        int max = -999, min = 999;
 
-        for(int i = 0 ; i < 1000; i++){
-            int x = sc.nextInt();
-            if(x == 999 || x == -999){
+        for(int i = 0 ; i < 100; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 999 || arr[i] == -999){
                 break;
             }else{
-                if(x >= max){
-                    max = x;
+                if(arr[i] >= max){
+                    max = arr[i];
                 }
-                if(x <= min){
-                    min = x;
+                if(arr[i] <= min){
+                    min = arr[i];
+                    //System.out.println("min = " + min);
                 }
             }
         }
