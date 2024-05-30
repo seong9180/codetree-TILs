df9730b32e74fd42c9144ec4cf17bdb6208bf9ae
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-05-30 17:25:26 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240530/연속부분수열일까/contiguous-array-or-not.java
@@ -0,0 +1,51 @@
+//package study.codetree.low;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int n1, n2;
+    public static final int MAX_N1 = 100, MAX_N2 = 100;
+    public static int[] arr1 = new int[MAX_N1];
+    public static int[] arr2 = new int[MAX_N2];
+
+    public static boolean isSequentSubString() {
+        boolean check = false;
+        for (int i = 0; i < n1; i++) {
+            if (arr1[i] == arr2[0]) { //연속 부분수열 판별이기 떄문에 무조건 arr2의 시작은 0번째부터
+                for (int j = 0; j < n2; j++) {
+                    if (arr1[i + j] == arr2[j]) {
+                        check = true;
+                    }else{
+                        check = false;
+                    }
+                }
+                if (check) { //b수열 전부를 비교한 뒤 부분수열이 맞으면
+                    break;
+                }
+            }
+        }
+        return check;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        if (isSequentSubString()) {
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+}
