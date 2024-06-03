28b98eb90805fe6423776e06c1f1fd1a9da97c22
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-03 16:17:23 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240603/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 240603/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -11,11 +11,17 @@ public class Main {
         }
 
         char word = sc.next().charAt(0);
+        boolean exist = false;
 
         for(int i = 0 ; i< 10; i++){
             if(string[i].charAt(string[i].length() - 1) == word){
+                exist = true;
                 System.out.println(string[i]);
             }
+        }
+
+        if(!exist){
+            System.out.println("None");
         }
     }
 }
