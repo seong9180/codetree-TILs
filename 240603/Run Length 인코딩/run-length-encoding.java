ae4661d5db2ec10e1c897da9555a428d233a8093
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-03 18:18:48 +0900

    Title: Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240603/Run Length 인코딩/run-length-encoding.java
+++ 240603/Run Length 인코딩/run-length-encoding.java
@@ -17,7 +17,15 @@ public class Main {
         }
 
         if(same){
-            digit_count = 2;
+            if(a.length() >= 10 && a.length() < 100){
+                digit_count = 3;
+            }else if(100 <= a.length() && a.length() < 1000){
+                digit_count = 4;
+            }else if(a.length() = 1000){
+                digit_count = 5;
+            }else{
+                digit_count = 2;
+            }
         }else{
             for(int i = 0 ; i < a.length(); i++){
                 if(i+1 < a.length() && a.charAt(i) != a.charAt(i+1)){
