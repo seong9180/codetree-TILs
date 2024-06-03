6cfbcd1cb5078e02e9d72f70c81080f213b1b11d
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-03 16:03:05 +0900

    Title: 단어로 구분하기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240603/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] string = new String[10];
+
+        for(int i = 0 ; i < 10; i++){
+            string[i] = sc.next();
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(string[i]);
+        }
+    }
+}
