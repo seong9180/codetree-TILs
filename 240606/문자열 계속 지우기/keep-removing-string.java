e05def9744db50f0e9f80d8a9f8ae329e1652422
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-06 18:55:18 +0900

    Title: 문자열 계속 지우기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240606/문자열 계속 지우기/keep-removing-string.java
+++ 240606/문자열 계속 지우기/keep-removing-string.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static int firstIndexOfSameSubstring(String a, String b) {
         //겹치는 부분 문자열의 시작 인덱스를 반환하는 함수 1 (겹치는 부분이 없다면 -1 출력)
+        //"인덱스" 를 반환하기 때문에 별도의 변경이 필요 없이 바로 사용하면 됨
         int main_length = a.length();
         int obj_length = b.length();
 
@@ -41,19 +42,19 @@ public class Main {
         2. 삭제
         3. 지우고 난 후 떨어진 a 의 문자열 다시 붙이기
         4. 새로운 문자열 a 에서 b 와 같은 부분 문자열 없어질 때까지 반복하기
-         */
+        */
 
         //문자열 a,b 선언
         String A = sc.next();
         String B = sc.next();
 
         while (true) {
-            if (firstIndexOfSameSubstring(A, B) == -1) {
-                System.out.println(A); //문자열 a 출력
+            if (firstIndexOfSameSubstring(A, B) == -1) { //겹치는 부분문자열이 없다면
+                System.out.println(A); //문자열 a 출력하고 루프 종료
                 break;
             }else{
-                int idx = firstIndexOfSameSubstring(A,B);
-                A = newCombinedString(idx, A, B);
+                int idx = firstIndexOfSameSubstring(A,B); //부분문자열의 시작 인덱스 저장
+                A = newCombinedString(idx, A, B); //문자열a 재정의
             }
         }
     }
