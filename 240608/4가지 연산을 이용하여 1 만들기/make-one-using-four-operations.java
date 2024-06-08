cfa7e2fb4e2b6144052a895a8ebf67822a9feb25
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-08 23:28:48 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 178ms, Memory: 10MB, Status: Passed - CodeTree

--- 240608/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 240608/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -8,41 +8,54 @@ public class Main {
         
         // BFS를 위한 큐 생성
         Queue<Integer> queue = new LinkedList<>();
-        // 최소 연산 횟수를 저장할 배열 생성
-        int[] dp = new int[N + 1];
+        // 최소 연산 횟수를 저장할 맵 생성
+        Map<Integer, Integer> dp = new HashMap<>();
         
-        // 배열 초기화
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[N] = 0;
-        
+        dp.put(N, 0);
         queue.add(N);
         
         while (!queue.isEmpty()) {
             int current = queue.poll();
             
+            // 1에 도달하면 결과를 출력하고 종료
             if (current == 1) {
-                System.out.println(dp[current]);
+                System.out.println(dp.get(current));
                 return;
             }
             
-            if (current - 1 > 0 && dp[current - 1] > dp[current] + 1) {
-                dp[current - 1] = dp[current] + 1;
-                queue.add(current - 1);
+            int nextValue;
+            
+            // 현재 숫자에서 가능한 연산을 수행하고, 최소 연산 횟수를 갱신
+            // 1 빼기
+            nextValue = current - 1;
+            if (!dp.containsKey(nextValue) || dp.get(nextValue) > dp.get(current) + 1) {
+                dp.put(nextValue, dp.get(current) + 1);
+                queue.add(nextValue);
+            }
+
+            // 1 더하기
+            nextValue = current + 1;
+            if (!dp.containsKey(nextValue) || dp.get(nextValue) > dp.get(current) + 1) {
+                dp.put(nextValue, dp.get(current) + 1);
+                queue.add(nextValue);
             }
             
-            if (current + 1 <= N && dp[current + 1] > dp[current] + 1) {
-                dp[current + 1] = dp[current] + 1;
-                queue.add(current + 1);
+            // 2로 나누기
+            if (current % 2 == 0) {
+                nextValue = current / 2;
+                if (!dp.containsKey(nextValue) || dp.get(nextValue) > dp.get(current) + 1) {
+                    dp.put(nextValue, dp.get(current) + 1);
+                    queue.add(nextValue);
+                }
             }
             
-            if (current % 2 == 0 && dp[current / 2] > dp[current] + 1) {
-                dp[current / 2] = dp[current] + 1;
-                queue.add(current / 2);
-            }
-            
-            if (current % 3 == 0 && dp[current / 3] > dp[current] + 1) {
-                dp[current / 3] = dp[current] + 1;
-                queue.add(current / 3);
+            // 3로 나누기
+            if (current % 3 == 0) {
+                nextValue = current / 3;
+                if (!dp.containsKey(nextValue) || dp.get(nextValue) > dp.get(current) + 1) {
+                    dp.put(nextValue, dp.get(current) + 1);
+                    queue.add(nextValue);
+                }
             }
         }
     }
