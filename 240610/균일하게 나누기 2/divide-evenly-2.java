a823d4c119cdfad8bb3a50de4580f10be3b5ce2e
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-10 22:26:31 +0900

    Title: 균일하게 나누기 2, Time: 1128ms, Memory: 14MB, Status: Passed - CodeTree

--- 240610/균일하게 나누기 2/divide-evenly-2.java
+++ 240610/균일하게 나누기 2/divide-evenly-2.java
@@ -8,17 +8,25 @@ public class Main {
         int[] x = new int[N];
         int[] y = new int[N];
         
+        Set<Integer> uniqueX = new TreeSet<>();
+        Set<Integer> uniqueY = new TreeSet<>();
+        
         for (int i = 0; i < N; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
+            uniqueX.add(x[i]);
+            uniqueY.add(y[i]);
         }
+        
+        Integer[] uniqueXArray = uniqueX.toArray(new Integer[0]);
+        Integer[] uniqueYArray = uniqueY.toArray(new Integer[0]);
         
         int result = N;
         
-        for (int xi = 0; xi < N; xi++) {
-            for (int yi = 0; yi < N; yi++) {
-                int a = x[xi] + 1;  // 짝수로 만들기 위해 +1
-                int b = y[yi] + 1;  // 짝수로 만들기 위해 +1
+        for (int xi : uniqueXArray) {
+            for (int yi : uniqueYArray) {
+                int a = xi + 1;  // 짝수로 만들기 위해 +1
+                int b = yi + 1;  // 짝수로 만들기 위해 +1
                 
                 int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
                 
