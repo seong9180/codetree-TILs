53eed30c7d55d9f931cf7c16253711d73bdf4742
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-14 19:34:48 +0900

    Title: 알파벳 지우기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240614/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        char[] char_a = a.toCharArray();
+        char[] char_b = b.toCharArray();
+
+        String new_a = "";
+        String new_b = "";
+
+        for(int i = 0; i < char_a.length; i++){
+            int asc_num = (int)char_a[i];
+
+            if(asc_num >= 48 && asc_num <= 57){
+                new_a += char_a[i];
+            }
+        }
+
+        for(int i = 0; i < char_b.length; i++){
+            int asc_num = (int)char_b[i];
+
+            if(asc_num >= 48 && asc_num <= 57){
+                new_b += char_b[i];
+            }
+        }
+
+        int sum = Integer.parseInt(new_a) + Integer.parseInt(new_b);
+        System.out.println(sum);
+    }
+}
