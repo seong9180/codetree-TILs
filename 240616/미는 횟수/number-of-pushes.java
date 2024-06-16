46687878c0fd7eadfa9b07494d0fe1783f0d9be6
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-16 20:23:55 +0900

    Title: 미는 횟수, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240616/미는 횟수/number-of-pushes.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        int n = 0; //n == count
+        int length = a.length();
+        for(int i = 0; i < length; i ++){
+            a = shiftRight(a);
+            //System.out.println(n + "번째 " +a);
+            if(a.equals(b)){
+                n ++;
+                break;
+            }
+            n++;
+        }
+        
+        if(n > length){ // 불가능
+            System.out.println(-1);
+        }else{
+            System.out.println(n);
+        }
+    }
+
+    public static String shiftRight(String a){
+        int length = a.length();
+        return a.charAt(length-1) + a.substring(0,length-1);
+    }
+}
