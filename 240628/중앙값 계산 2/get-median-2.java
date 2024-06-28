0884d81d1534fe9affb662685f725496efa83079
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-06-28 12:46:10 +0900

    Title: 중앙값 계산 2, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240628/중앙값 계산 2/get-median-2.java
+++ 240628/중앙값 계산 2/get-median-2.java
@@ -8,11 +8,11 @@ public class Main {
     public static int[] arr = new int[MAX_N];
 
     public static void middleValueOut(int idx, int[] arr) {
-        int index = 0;
-        if(idx != 0){
-            index = idx / 2 + 1; //idx / 2 => 중간 값 인덱스 ;; 왜 + 1 해야 맞지?
-        }
-
+        int index = idx / 2; //idx / 2 => 중간 값 인덱스
+        // idx == 3, {1, 5, 2} , index == 2
+        // 중간값 => 2
+        Arrays.sort(arr, 0, idx + 1); //idx 가 이미 인덱스 값이므로, sort 에는 idx + 1
+        //System.out.print("index = " + index + " ");
         System.out.print(arr[index] + " ");
     }
 
@@ -22,10 +22,11 @@ public class Main {
         n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            if (i % 2 != 0 || i == 0) { //i == 0 or i 가 홀수 일 때
+            if (i % 2 == 0) { //순서상으론 i = 0 이 첫번째 (1 번째) 따라서 짝수여야 함
                 middleValueOut(i,arr);
                 //System.out.println(arr.length);
             }
         }
+        sc.close();
     }
 }
