f804cbff5b3c23852c90bf3c37e38fc1b1b87ded
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-07-03 20:53:08 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 321ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240703/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int P = scanner.nextInt();
+        int T = scanner.nextInt();
+        
+        List<Handshake> handshakes = new ArrayList<>();
+        for (int i = 0; i < T; i++) {
+            int t = scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            handshakes.add(new Handshake(t, x, y));
+        }
+        
+        Collections.sort(handshakes, Comparator.comparingInt(h -> h.time));
+        
+        boolean[] infected = new boolean[N + 1];
+        int[] infectionCount = new int[N + 1];
+        
+        infected[P] = true;
+        
+        for (Handshake handshake : handshakes) {
+            int x = handshake.x;
+            int y = handshake.y;
+            
+            if (infected[x] && infectionCount[x] < K) {
+                infected[y] = true;
+                infectionCount[x]++;
+            }
+            if (infected[y] && infectionCount[y] < K) {
+                infected[x] = true;
+                infectionCount[y]++;
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            result.append(infected[i] ? 1 : 0);
+        }
+        
+        System.out.println(result.toString());
+    }
+}
+
+class Handshake {
+    int time;
+    int x;
+    int y;
+    
+    Handshake(int time, int x, int y) {
+        this.time = time;
+        this.x = x;
+        this.y = y;
+    }
+}
