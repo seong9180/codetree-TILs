7e010c49c94140168698bdef17485cfb72120182
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-07-05 16:13:03 +0900

    Title: Date to Date, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240705/Date to Date/date-to-date.java
+++ 240705/Date to Date/date-to-date.java
@@ -22,15 +22,10 @@ public class Main {
         //시작 월, 일 m1, d1
         //(m1 월의 최대일 - d1) + 그 사이 월
         int exclusive_day = 1; //첫 날 포함해서 계산하기 때문에 0 > 1
-        if(m1 == m2 && d1 == d2){ //동일한 날이 주어지면,
-            return exclusive_day; //함수 종료
-            //동일한 날이면 gap == 0 케이스에 들어가 d2 - d1 을 리턴하기 때문에
-            //시작일을 포함해 1이 나오는 게 아닌 0이 나오게 됨
-        }
         //1. m1 과 m2의 차이 구하기
         int gap = m2 - m1;
         if(gap == 0){ //같은 월일때
-            exclusive_day = d2 - d1; 
+            exclusive_day += d2 - d1; 
         }else if(gap >= 2){ //사이에 한개 이상의 달이 껴 있는 경우
             for(int i = 2; i <= gap; i++){
                 exclusive_day += countMonth(m1 + i - 1);
