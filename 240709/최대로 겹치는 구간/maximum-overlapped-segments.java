e8de0b0192ba05ba304ce344febb548915bfa665
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-07-09 21:34:02 +0900

    Title: 최대로 겹치는 구간, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240709/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240709/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -17,7 +17,7 @@ class LineSegment { //선분을 저장해 둘 선분 클래스 지정
 }
 
 public class Main {
-    public static int[] maxArea = new int[200]; // offset 반영 최대 크기 200 번위 설정
+    public static int[] maxArea = new int[202]; // offset 반영 최대 크기 200 번위 설정
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -28,18 +28,19 @@ public class Main {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            //x,y 의 범위가 -100 ~ +100 이라서 전체적으로 + 100 offset 설정
-            segments[i] = new LineSegment(x + 100, y + 100);
+            //x,y 의 범위가 -100 ~ +100 이라서 전체적으로 + 101 offset 설정
+            //배열 0~200++ 범위에 넣기 위해 + 101로 수정.
+            segments[i] = new LineSegment(x + 101, y + 101);
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = segments[i].x ; j < segments[i].y - 1; j++) { //구간 카운트는 x1 ~ x2 -1
+            for (int j = segments[i].x - 1 ; j < segments[i].y - 1; j++) { //구간 카운트는 x1 ~ x2 -1
                 maxArea[j] ++;
             }
         }
 
         int max = 0;
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 202; i++) {
             if (maxArea[i] >= max) {
                 max = maxArea[i];
             }
