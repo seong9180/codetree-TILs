c09ec9078ac213eea9c86e28e2b02798e7801587
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-07-20 19:17:06 +0900

    Title: 단순한 동전 챙기기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240720/단순한 동전 챙기기/collect-coins-easy.java
+++ 240720/단순한 동전 챙기기/collect-coins-easy.java
@@ -1,88 +1,87 @@
-import java.io.*;
 import java.util.*;
-
-class Point {
-    int x, y, coins, moves;
-    
-    Point(int x, int y, int coins, int moves) {
-        this.x = x;
-        this.y = y;
-        this.coins = coins;
-        this.moves = moves;
-    }
-}
 
 public class Main {
     static int N;
     static char[][] grid;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static Point start, end;
-    static List<Point> coinList = new ArrayList<>();
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        grid = new char[N][N];
+    static int[][] coins;
+    static int coinCount;
+    static int startX, startY, endX, endY;
+    static final int INF = 1000000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        sc.nextLine();
         
+        grid = new char[N][N];
+        coins = new int[10][2];
+        coinCount = 0;
+
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
+            String line = sc.nextLine();
             for (int j = 0; j < N; j++) {
                 grid[i][j] = line.charAt(j);
-                if (grid[i][j] == 'S') start = new Point(i, j, 0, 0);
-                else if (grid[i][j] == 'E') end = new Point(i, j, 0, 0);
-                else if (grid[i][j] >= '1' && grid[i][j] <= '9') {
-                    coinList.add(new Point(i, j, 1 << (grid[i][j] - '1'), 0));
+                if (grid[i][j] == 'S') {
+                    startX = i;
+                    startY = j;
+                } else if (grid[i][j] == 'E') {
+                    endX = i;
+                    endY = j;
+                } else if (Character.isDigit(grid[i][j])) {
+                    coins[grid[i][j] - '0'][0] = i;
+                    coins[grid[i][j] - '0'][1] = j;
+                    coinCount++;
                 }
             }
         }
-        
-        if (coinList.size() < 3) {
-            System.out.println(-1);
-            return;
-        }
-        
-        Collections.sort(coinList, (a, b) -> Integer.bitCount(b.coins) - Integer.bitCount(a.coins));
-        
-        int result = bfs();
+
+        int result = solve();
         System.out.println(result);
     }
-    
-    static int bfs() {
-        Queue<Point> queue = new LinkedList<>();
-        boolean[][][] visited = new boolean[N][N][1 << coinList.size()];
-        queue.offer(start);
-        visited[start.x][start.y][0] = true;
-        
-        while (!queue.isEmpty()) {
-            Point current = queue.poll();
-            
-            if (current.x == end.x && current.y == end.y && Integer.bitCount(current.coins) >= 3) {
-                return current.moves;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
-                
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                
-                int newCoins = current.coins;
-                if (grid[nx][ny] >= '1' && grid[nx][ny] <= '9') {
-                    int coinIndex = grid[nx][ny] - '1';
-                    if ((newCoins & (1 << coinIndex)) == 0 && 
-                        (newCoins == 0 || coinIndex > Integer.numberOfTrailingZeros(newCoins))) {
-                        newCoins |= (1 << coinIndex);
+
+    static int solve() {
+        int minMoves = INF;
+        for (int i = 1; i <= coinCount - 2; i++) {
+            for (int j = i + 1; j <= coinCount - 1; j++) {
+                for (int k = j + 1; k <= coinCount; k++) {
+                    int moves = bfs(startX, startY, coins[i][0], coins[i][1]);
+                    moves += bfs(coins[i][0], coins[i][1], coins[j][0], coins[j][1]);
+                    moves += bfs(coins[j][0], coins[j][1], coins[k][0], coins[k][1]);
+                    moves += bfs(coins[k][0], coins[k][1], endX, endY);
+                    if (moves < minMoves) {
+                        minMoves = moves;
                     }
-                }
-                
-                if (!visited[nx][ny][newCoins]) {
-                    visited[nx][ny][newCoins] = true;
-                    queue.offer(new Point(nx, ny, newCoins, current.moves + 1));
                 }
             }
         }
-        
-        return -1;
+        return minMoves == INF ? -1 : minMoves;
+    }
+
+    static int bfs(int startX, int startY, int endX, int endY) {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[N][N];
+        queue.offer(new int[]{startX, startY, 0});
+        visited[startX][startY] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0], y = current[1], moves = current[2];
+
+            if (x == endX && y == endY) {
+                return moves;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
+                    queue.offer(new int[]{nx, ny, moves + 1});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+        return INF;
     }
 }
