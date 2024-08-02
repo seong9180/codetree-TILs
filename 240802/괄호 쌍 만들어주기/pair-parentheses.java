f5605fbaa275b6a7db9311fbd8cfe9f99d335157
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-02 21:03:59 +0900

    Title: 괄호 쌍 만들어주기, Time: 302ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 240802/괄호 쌍 만들어주기/pair-parentheses.java
+++ 240802/괄호 쌍 만들어주기/pair-parentheses.java
@@ -8,30 +8,23 @@ public class Main {
         System.out.println(countBracketPairs(brackets));
     }
     
-    public static long countBracketPairs(String brackets) {
+    public static int countBracketPairs(String brackets) {
         List<Integer> openPairs = new ArrayList<>();
         List<Integer> closePairs = new ArrayList<>();
         
-        int openCount = 0;
-        int closeCount = 0;
-        
-        // 연속된 여는 괄호와 닫는 괄호의 시작 위치를 찾습니다.
-        for (int i = 0; i < brackets.length(); i++) {
-            if (brackets.charAt(i) == '(') {
-                if (openCount == 0) openPairs.add(i);
-                openCount++;
-                closeCount = 0;
-            } else {
-                if (closeCount == 0) closePairs.add(i);
-                closeCount++;
-                openCount = 0;
+        // 연속된 두 개의 여는 괄호와 닫는 괄호의 위치를 찾습니다.
+        for (int i = 0; i < brackets.length() - 1; i++) {
+            if (brackets.charAt(i) == '(' && brackets.charAt(i+1) == '(') {
+                openPairs.add(i);
+            } else if (brackets.charAt(i) == ')' && brackets.charAt(i+1) == ')') {
+                closePairs.add(i);
             }
         }
         
-        long count = 0;
+        int count = 0;
         int openIndex = 0;
         
-        // 각 닫는 괄호 그룹에 대해 가능한 여는 괄호 그룹의 수를 계산합니다.
+        // 각 닫는 괄호 쌍에 대해 가능한 여는 괄호 쌍의 수를 계산합니다.
         for (int closePos : closePairs) {
             while (openIndex < openPairs.size() && openPairs.get(openIndex) < closePos) {
                 openIndex++;
