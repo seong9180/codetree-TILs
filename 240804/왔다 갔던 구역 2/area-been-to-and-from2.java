c99591bb9e649d077c0a0d114949b5d87165cdd1
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-04 18:59:09 +0900

    Title: 왔다 갔던 구역 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240804/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240804/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,36 +1,35 @@
-//package NoviceMid.Simulation1;
-
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt(); // 명령 갯수
-        int[] visited = new int[2000]; //시작점 1000, 앞뒤로 1000씩 이동 가능하게 (10 * 100)
-        int standard = 1000; // 기준점을 1000에 설정.
+        HashMap<Integer, Integer> visited = new HashMap<>();
+        int position = 0; // 현재 위치
 
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0); //L or R
 
             if (dir == 'L') {
-                for (int j = standard; j > standard - x; j--) {
-                    visited[j]++;
+                for (int j = position - 1; j > position - x; j--) {
+                    visited.put(j, visited.getOrDefault(j, 0) + 1);
                 }
-                standard -= x; //이동한 거리대로 기준점 수정
-            }else{
-                for (int j = standard; j < standard + x; j++) {
-                    visited[j]++;
+                position -= x;
+            } else {
+                for (int j = position; j < position + x - 1; j++) {
+                    visited.put(j, visited.getOrDefault(j, 0) + 1);
                 }
-                standard += x; //이동한 거리대로 기준점 수정
+                position += x - 1;
             }
         }
 
         int areaCount = 0;
-        for (int i = 0; i < 2000; i++) {
-            if (visited[i] >= 2) { //2번 이상 지나갔다면,
-                areaCount++; //영역의 크기 증가
+        for (int count : visited.values()) {
+            if (count >= 2) {
+                areaCount++;
             }
         }
 
