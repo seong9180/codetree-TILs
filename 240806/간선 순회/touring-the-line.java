147d562ac30e5a82a42e6db5d146ce7b83cbd405
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-06 21:29:56 +0900

    Title: 간선 순회, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240806/간선 순회/touring-the-line.java
+++ 240806/간선 순회/touring-the-line.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Main {
     static List<List<Edge>> tree;
     static int d;
-    static int totalDistance;
-    static int maxDistance;
+    static int[] dist;
+    static int farthestNode;
 
     static class Edge {
         int to;
@@ -26,25 +26,33 @@ public class Main {
             tree.add(new ArrayList<>());
         }
 
-        totalDistance = 0;
         for (int i = 0; i < n - 1; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             int length = sc.nextInt();
             tree.get(u).add(new Edge(v, length));
             tree.get(v).add(new Edge(u, length));
-            totalDistance += length;
         }
 
-        maxDistance = 0;
+        // Find the farthest node from node 1
+        dist = new int[n + 1];
         dfs(1, -1, 0);
 
-        int days = (int) Math.ceil((double) totalDistance / d);
+        // Find the farthest node from the previously found farthest node
+        int start = farthestNode;
+        dist = new int[n + 1];
+        dfs(start, -1, 0);
+
+        int diameter = dist[farthestNode];
+        int days = (diameter + d - 1) / d;
         System.out.println(Math.max(days, 2));
     }
 
     static void dfs(int node, int parent, int distance) {
-        maxDistance = Math.max(maxDistance, distance);
+        dist[node] = distance;
+        if (distance > dist[farthestNode]) {
+            farthestNode = node;
+        }
 
         for (Edge edge : tree.get(node)) {
             if (edge.to != parent) {
