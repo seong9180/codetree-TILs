02517f7f4827cdca79e14ed92c2c043adb7c91b6
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-08 20:17:38 +0900

    Title: 대응되는 수와 문자, Time: 3067ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 240808/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 240808/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -1,34 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // 입력 받기
-        String[] input = scanner.nextLine().split(" ");
+        String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
 
         // 문자열과 숫자의 대응 관계 저장
         String[] stringToNumber = new String[n + 1];
         for (int i = 1; i <= n; i++) {
-            stringToNumber[i] = scanner.nextLine();
+            stringToNumber[i] = br.readLine();
         }
 
         // 조사할 문자열 또는 숫자에 대한 결과 출력
         for (int i = 0; i < m; i++) {
-            String query = scanner.nextLine();
+            String query = br.readLine();
             if (Character.isDigit(query.charAt(0))) {
                 int number = Integer.parseInt(query);
-                System.out.println(stringToNumber[number]);
+                bw.write(stringToNumber[number] + "\n");
             } else {
                 for (int j = 1; j <= n; j++) {
                     if (stringToNumber[j].equals(query)) {
-                        System.out.println(j);
+                        bw.write(String.valueOf(j) + "\n");
                         break;
                     }
                 }
             }
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
