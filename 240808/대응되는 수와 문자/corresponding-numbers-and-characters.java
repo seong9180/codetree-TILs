5def048c4ca5a2de975f292eac7d1e2b61fd221c
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-08 20:22:21 +0900

    Title: 대응되는 수와 문자, Time: 2008ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240808/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 240808/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -7,41 +7,25 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         sc.nextLine(); // 개행 문자 처리
-        
-        // 두 개의 HashMap 생성
-        HashMap<Integer, String> numToStrMap = new HashMap<>();
-        HashMap<String, Integer> strToNumMap = new HashMap<>();
+
+        // 하나의 HashMap만 사용
+        HashMap<String, String> map = new HashMap<>();
         
         // n개의 문자열을 입력받아 HashMap에 저장
         for (int i = 1; i <= n; i++) {
             String str = sc.nextLine();
-            numToStrMap.put(i, str);
-            strToNumMap.put(str, i);
+            // 숫자 -> 문자열
+            map.put(String.valueOf(i), str);
+            // 문자열 -> 숫자
+            map.put(str, String.valueOf(i));
         }
         
         // m개의 조사할 값을 입력받아 대응되는 값을 출력
         for (int i = 0; i < m; i++) {
             String query = sc.nextLine();
-            
-            // 입력이 숫자인지 문자열인지 판단
-            if (isNumeric(query)) {
-                int num = Integer.parseInt(query);
-                System.out.println(numToStrMap.get(num));
-            } else {
-                System.out.println(strToNumMap.get(query));
-            }
+            System.out.println(map.get(query));
         }
-        
+
         sc.close();
-    }
-    
-    // 입력된 문자열이 숫자인지 확인하는 메소드
-    public static boolean isNumeric(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
     }
 }
