108f2040947c9f3a9a4ddf962b11899cf307b19e
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-16 21:15:29 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 340ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240816/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
+++ 240816/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
@@ -1,36 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static class Bracket implements Comparable<Bracket> {
+        int open, close;
+        String str;
+        
+        Bracket(String s) {
+            this.str = s;
+            for (char c : s.toCharArray()) {
+                if (c == '(') open++;
+                else close++;
+            }
+        }
+        
+        @Override
+        public int compareTo(Bracket other) {
+            int thisUnmatched = open - close;
+            int otherUnmatched = other.open - other.close;
+            if (thisUnmatched != otherUnmatched) {
+                return otherUnmatched - thisUnmatched;
+            }
+            return other.close - this.close;
+        }
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        List<String> brackets = new ArrayList<>();
+        List<Bracket> brackets = new ArrayList<>();
+        
         for (int i = 0; i < n; i++) {
-            brackets.add(br.readLine());
+            brackets.add(new Bracket(br.readLine()));
         }
         
-        // 각 문자열에 대해 '('의 개수에서 ')'의 개수를 뺀 값을 기준으로 정렬
-        brackets.sort((a, b) -> {
-            int diffA = countChar(a, '(') - countChar(a, ')');
-            int diffB = countChar(b, '(') - countChar(b, ')');
-            return diffB - diffA;
-        });
+        Collections.sort(brackets);
         
         StringBuilder sb = new StringBuilder();
-        for (String s : brackets) {
-            sb.append(s);
+        for (Bracket b : brackets) {
+            sb.append(b.str);
         }
         
         String result = sb.toString();
         long score = calculateScore(result);
         
         System.out.println(score);
-    }
-    
-    private static int countChar(String s, char c) {
-        return (int) s.chars().filter(ch -> ch == c).count();
     }
     
     private static long calculateScore(String s) {
