c6ce466f1439d81a56f9a175c32d2e1ef6b0b38c
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-17 21:33:03 +0900

    Title: 적석과 흑석 2, Time: 1043ms, Memory: 43MB, Status: Passed - CodeTree

--- 240817/적석과 흑석 2/red-stone-and-black-stone-2.java
+++ 240817/적석과 흑석 2/red-stone-and-black-stone-2.java
@@ -2,66 +2,58 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int C, N;
-    static int[] redStones;
-    static int[][] blackStones;
-    static ArrayList<Integer>[] graph;
-    static int[] matched;
-    static boolean[] visited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        C = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         
-        redStones = new int[C];
+        int[] redStones = new int[C];
         for (int i = 0; i < C; i++) {
             redStones[i] = Integer.parseInt(br.readLine());
         }
         
-        blackStones = new int[N][2];
+        Arrays.sort(redStones);
+        
+        TreeSet<Integer> availableReds = new TreeSet<>();
+        for (int i = 0; i < C; i++) {
+            availableReds.add(i);
+        }
+        
+        int[][] blackStones = new int[N][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             blackStones[i][0] = Integer.parseInt(st.nextToken());
             blackStones[i][1] = Integer.parseInt(st.nextToken());
         }
         
-        graph = new ArrayList[C];
-        for (int i = 0; i < C; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
-        for (int i = 0; i < C; i++) {
-            for (int j = 0; j < N; j++) {
-                if (blackStones[j][0] <= redStones[i] && redStones[i] <= blackStones[j][1]) {
-                    graph[i].add(j);
-                }
-            }
-        }
-        
-        matched = new int[N];
-        Arrays.fill(matched, -1);
+        Arrays.sort(blackStones, (a, b) -> a[1] - b[1]);
         
         int count = 0;
-        for (int i = 0; i < C; i++) {
-            visited = new boolean[N];
-            if (dfs(i)) count++;
+        for (int[] black : blackStones) {
+            Integer redIndex = availableReds.ceiling(
+                lowerBound(redStones, black[0])
+            );
+            if (redIndex != null && redIndex < C && redStones[redIndex] <= black[1]) {
+                count++;
+                availableReds.remove(redIndex);
+            }
         }
         
         System.out.println(count);
     }
     
-    static boolean dfs(int red) {
-        for (int black : graph[red]) {
-            if (visited[black]) continue;
-            visited[black] = true;
-            if (matched[black] == -1 || dfs(matched[black])) {
-                matched[black] = red;
-                return true;
+    private static int lowerBound(int[] arr, int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return false;
+        return left;
     }
 }
