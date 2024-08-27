b4e69cb595395e5864b9c98e26d039faef46df5f
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-08-27 16:43:05 +0900

    Title: 선두를 지켜라, Time: 305ms, Memory: 29MB, Status: Passed - CodeTree

--- 240827/선두를 지켜라/keep-the-lead.java
+++ 240827/선두를 지켜라/keep-the-lead.java
@@ -22,39 +22,50 @@ public class Main {
             int velocity = sc.nextInt(); //속력
             int movingTime = sc.nextInt(); //이동거리
             for (int j = aTime; j < aTime + movingTime; j++) {
-                aPos = velocity * j;
+                aPos += velocity;
                 arrA[j] = aPos;
             }
             totalTime += movingTime;
+            aTime += movingTime;
         }
 
         for (int i = 0; i < m; i++) {
             int velocity = sc.nextInt(); //속력
             int movingTime = sc.nextInt(); //이동거리
             for (int j = bTime; j < bTime + movingTime; j++) {
-                bPos = velocity * j;
+                bPos += velocity;
                 arrB[j] = bPos;
             }
+            bTime += movingTime;
         }
 
         int count = 0; //선두 변경 횟수 카운트
-        boolean check = true; //true : A 선두, false : B 선두
+        //boolean check = true; //true : A 선두, false : B 선두
+        int check = 0; //공동 선두, 1 : a 선두, 2 : b 선두
         boolean exist = false;
         for (int i = 0; i < totalTime; i++) {
-            if (arrB[i] > arrA[i] && check) {
+            if (arrA[i] == arrB[i]) {
+                check = 0;
+            }
+            else if (arrB[i] > arrA[i] && check != 2) {
                 exist = true;
-                check = false;
+                check = 2;
                 count ++;
-            } else if (arrA[i] > arrB[i] && !check) {
+            } else if (arrA[i] > arrB[i] && check != 1) {
                 exist = true;
-                check = true;
+                check = 1;
                 count ++;
             }
         }
         if (!exist) {
             System.out.println(-1);
         } else {
-            System.out.println(count);
+            System.out.println(count - 1);
+//            if (arrA[1] != arrB[1]) { //시작 선두가 정해져 있었다면
+//                System.out.println(count - 1);
+//            } else {
+//                System.out.println(count);
+//            }
         }
     }
 }
