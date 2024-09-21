002de63985e627ca0f3e5ec9e477112922c4a5ad
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2024-09-21 11:17:41 +0900

    Title: Carry 피하기 2, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240921/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,61 @@
+//package NoviceMid.ExhaustiveSearch;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int n;
+    public static final int MAX_N = 20;
+    public static final int MAX = 10000;
+    public static int[] input = new int[MAX_N];
+
+    public static boolean isCarry(int a, int b, int c) {
+        int[] arrA = new int[5]; //최대값이 10000, 자릿수 기준으로 배열은 5 크기로 생성
+        int[] arrB = new int[5];
+        int[] arrC = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            if (a > 0) {
+                arrA[i] = a % 10;
+                a /= 10;
+            }if (b > 0) {
+                arrB[i] = b % 10;
+                b /= 10;
+            }if (c > 0) {
+                arrC[i] = c % 10;
+                c /= 10;
+            }
+        }
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(arrA[i] + " " + arrB[i] + " " + arrC[i]);
+//        }
+
+        for (int i = 0; i < 5; i++) {
+            if (arrA[i] + arrB[i] + arrC[i] >= 10) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            input[i] = sc.nextInt();
+        }
+
+        int maxTotal = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (isCarry(input[i], input[j], input[k])) {
+                        maxTotal = Math.max(input[i] + input[j] + input[k], maxTotal);
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxTotal);
+    }
+}
