19e7b8c84b7f62b85dc273f5bd7b1015d481cb3d
seungmin.Kim <81938885+seong9180@users.noreply.github.com>
2025-03-13 14:50:54 +0900

    Title: DateTime to DateTime, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250313/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        // Please write your code here.
+        int start = (11*24 + 11)*60 + 11;
+        int end = (A*24 + B)*60 + C;
+        
+        System.out.println(end - start);
+    }
+}
